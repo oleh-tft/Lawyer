fdd02c75b71bf682345a7a180d7aeff549d230a3
oleh-tft <tiftelalbion@gmail.com>
2024-10-16 12:49:53 +0300

    Updated main.cpp architecture

--- Project3/Main.cpp
+++ Project3/Main.cpp
@@ -12,15 +12,21 @@ int main()
 {
 	Human* human = nullptr;
 	int pick = 0;
+	int action = 0;
 
-	cout << "1. Student info\t\t 2. Student scholarship" << endl;
-	cout << "3. Lawyer info\t\t 4. Lawyer salary" << endl;
-	cout << "5. Teacher info\t\t 6. Teacher salary" << endl;
-	cout << "7. Developer info\t 8. Developer salary" << endl;
-	cout << "9. Designer info\t 10. Designer salary" << endl;
-	cout << "11. Director info\t 12. Director salary" << endl << endl;
-	cout << "Choose option: ";
+	cout << "1. Student" << endl;
+	cout << "2. Lawyer" << endl;
+	cout << "3. Teacher" << endl;
+	cout << "4. Developer" << endl;
+	cout << "5. Designer" << endl;
+	cout << "6. Director" << endl;
+	cout << "Choose subject: ";
 	cin >> pick;
+
+	cout << "1. Show inforamtion" << endl;
+	cout << "2. Show salary" << endl;
+	cout << "Choose action: ";
+	cin >> action;
 
 	switch (pick)
 	{
@@ -28,46 +34,29 @@ int main()
 		human = new Student("Oleg", 19, "Itstep", 2000);
 		break;
 	case 2:
-		human = new Student("Oleg", 19, "Itstep", 2000);
+		human = new Lawyer("Daria", 21, "Custom", false, 40000.0);
 		break;
 	case 3:
-		human = new Lawyer("Daria", 21, "Custom", false, 40000.0);
+		human = new Teacher("Mark", 42, "Philosophy", 38999.50);
 		break;
 	case 4:
-		human = new Lawyer("Daria", 21, "Custom", false, 40000.0);
+		human = new Developer("Oleh", 22, "Java, C++", false, 999500.0);
 		break;
 	case 5:
-		human = new Teacher("Mark", 42, "Philosophy", 38999.50);
+		human = new Designer("Yan", 20, "Blender", "Moderate", 250000.0);
 		break;
 	case 6:
-		human = new Teacher("Mark", 42, "Philosophy", 38999.50);
-		break;
-	case 7:
-		human = new Developer("Oleh", 22, "Java, C++", false, 999500.0);
-		break;
-	case 8:
-		human = new Developer("Oleh", 22, "Java, C++", false, 999500.0);
-		break;
-	case 9:
-		human = new Designer("Yan", 20, "Blender", "Moderate", 250000.0);
-		break;
-	case 10:
-		human = new Designer("Yan", 20, "Blender", "Moderate", 250000.0);
-		break;
-	case 11:
-		human = new Director("Mary", 30, "Cool company", 40, 180909.0);
-		break;
-	case 12:
 		human = new Director("Mary", 30, "Cool company", 40, 180909.0);
 		break;
 	}
 
-	if (pick % 2 != 0)
+	switch (action)
 	{
+	case 1:
 		human->Print();
-	}
-	else
-	{
+		break;
+	case 2:
 		human->Salary();
+		break;
 	}
 }
